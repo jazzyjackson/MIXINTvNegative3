493f31aaa93bb24682a3a7b16d7a3a45d7a3d6a3
Colten Jackson <Colten Jackson>
2016-07-29 15:02:42 -0400

    Added server ability to follow relative links moved stylesheet to new file

--- simpleserver.js
+++ simpleserver.js
@@ -1,11 +1,14 @@
 var server = require('http').createServer(handler)
 var fs = require('fs')
+var url = require('url')
+
 
 server.listen(8080)
 
 function handler(request, response) {
-    fs.readFile(__dirname + '/index.html',
+    fs.readFile(__dirname + url.parse(request.url).pathname,
         function (err, data) {
+            console.log(url.parse(request.url).pathname)
             if (err) {
                 response.writeHead(500)
                 return response.end('Error loading index.html')
