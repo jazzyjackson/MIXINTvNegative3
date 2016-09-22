15b6266059b806924fdfa54806dfd579e5b4c591
Colten Jackson <Colten Jackson>
2016-09-21 22:39:09 -0400

    autofocus new element and better header styling

--- public/js/clickevents.js
+++ public/js/clickevents.js
@@ -6,14 +6,8 @@ document.documentElement.addEventListener('dblclick', function(event){
 
 document.documentElement.addEventListener('mousemove', function(event){
   event.preventDefault();
-  if(event.buttons && event.target === document.activeElement){
-    var currentXpos = Number(event.target.style.left.slice(0,-2));
-    var currentYpos = Number(event.target.style.top.slice(0,-2));
-    event.target.style.left = (currentXpos + event.movementX) + 'px';
-    event.target.style.top = (currentYpos + event.movementY) + 'px';
-    console.log(event);
-    console.log(currentXpos);
-    console.log(currentYpos);
+  if(event.buttons && event.target === document.activeElement && event.target.tagName === 'DIV'){
+    updatePos(event, document.activeElement);
   }
 })
 
@@ -21,6 +15,7 @@ document.documentElement.addEventListener('mousemove', function(event){
 function addTextool(event){
   var aTexTool = new TexTool(event.clientX, event.clientY);
   document.body.appendChild(aTexTool.element);
+  aTexTool.element.focus();
 }
 
 
@@ -31,9 +26,18 @@ function TexTool(xPos, yPos){
   this.element.style.left = xPos + 'px';
   this.element.style.top = yPos + 'px';
   this.element.tabIndex = 1;
-  this.element.focus();
   var texToolHeader = document.createElement('h5');
   texToolHeader.innerText = Date();
   this.element.appendChild(texToolHeader);
 }
+
+function updatePos(moveEvent, element){
+    var currentXpos = Number(moveEvent.target.style.left.slice(0,-2));
+    var currentYpos = Number(moveEvent.target.style.top.slice(0,-2));
+    element.style.left = (currentXpos + moveEvent.movementX) + 'px';
+    element.style.top = (currentYpos + moveEvent.movementY) + 'px';
+    console.log(event);
+    console.log(currentXpos);
+    console.log(currentYpos);
+};
 
