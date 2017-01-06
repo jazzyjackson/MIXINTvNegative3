15ab5b8cce05794f2ede2fbf62987a4d5f096409
Colten Jackson <Colten Jackson>
2017-01-06 11:53:57 -0500

    removed accidentally added html

--- grabvideo.js
+++
@@ -1,4 +0,0 @@
-var video = document.getElementById('innertag0').shadowRoot.getElementById('webcam')
-navigator.getUserMedia({video: true}, stream => video.src = URL.createObjectURL(stream), err => console.error.bind(console))
-
-// <video autoplay="true" id='webcam' width="100%" ></video>
